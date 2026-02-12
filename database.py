1406acc8708ad98f2e459ff56cb4c677ee38fb51
asd <asd>
2026-02-12 14:25:31 +0100

    adatbazis bovitese, register, profile, login button

--- database.py
+++ database.py
@@ -9,6 +9,7 @@ bcrypt = Bcrypt()
 class User(db.Model, UserMixin):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(80), unique=True, nullable=False)
+    email = db.Column(db.String(80), unique=True, nullable=False)
     password_hash = db.Column(db.String(128), nullable=False)
     created_at = db.Column(db.DateTime, default=datetime.now())
 
@@ -19,6 +20,29 @@ class User(db.Model, UserMixin):
         return bcrypt.check_password_hash(self.password_hash, password)
     
 
+
+
+class Item(db.Model):
+    id = db.Column(db.Integer, primary_key=True) 
+    name = db.Column(db.String(80), nullable=False) 
+    description =  db.Column(db.Text, nullable=False)
+    created_at = db.Column(db.DateTime, default=datetime.now)
+    attachments= db.relationship('Attachment', backref='item', lazy=True)
+    uploader_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+    active = db.Column(db.Boolean, default=True)
+    type = db.Column(db.String(20), default='lost') 
+    category_id = db.Column(db.Integer, db.ForeignKey('category.id'), nullable=True)
+
+class Attachment(db.Model):
+    id = db.Column(db.Integer, primary_key=True) 
+    filename = db.Column(db.String(255), nullable=False)
+    item_id = db.Column(db.Integer, db.ForeignKey('item.id'), nullable=False) 
+    created_at = db.Column(db.DateTime, default=datetime.now)
+
+class Category(db.Model):
+    id = db.Column(db.Integer, primary_key=True) 
+    name = db.Column(db.String(50), unique=True, nullable=False)
+    icon = db.Column(db.String(255), nullable=False)
 
 def init_db(app):
     with app.app_context():
--- routes.py
+++ routes.py
@@ -1,5 +1,5 @@
 from flask import Flask, jsonify, request, redirect, url_for, render_template
-from database import db, User
+from database import db, User, Item, Category, Attachment
 from flask_login import login_user, logout_user, login_required, current_user
 
 
@@ -7,18 +7,35 @@ from flask_login import login_user, logout_user, login_required, current_user
 def create_routes(app: Flask):
     @app.route('/')
     def home():
-        return render_template("home.html")
+        items = Item.query.filter_by(active=True).all()
+        return render_template("home.html", items=items)
     @app.route('/login', methods=['GET', 'POST'])
     def login():
         if request.method == 'POST':
-            username = request.form.get('username')
-            password = request.form.get('password')
-            user = User.query.filter_by(username=username).first()
-            if user and user.check_password(password):
-                login_user(user, remember=True)
+            action_type = request.form.get('type')
+            if action_type == 'login':
+                username = request.form.get('username')
+                password = request.form.get('passwd')
+                user = User.query.filter_by(username=username).first()
+                if user and user.check_password(password):
+                    login_user(user, remember=True)
+                    return redirect(url_for('home'))
+                else:
+                    return render_template("login.html")
+            elif action_type == 'register':
+                username = request.form.get('username') 
+                email = request.form.get('email') 
+                password = request.form.get('passwd') 
+                if User.query.filter_by(username=username).first(): 
+                    return render_template("login.html", error="Felhasznalonev foglalt") 
+                if User.query.filter_by(email=email).first(): 
+                    return render_template("login.html", error="email foglalt") 
+                new_user = User(username=username, email=email) 
+                new_user.set_password(password) 
+                db.session.add(new_user) 
+                db.session.commit() 
+                login_user(new_user, remember=True) 
                 return redirect(url_for('home'))
-            else:
-                return render_template("login.html")
         return render_template("login.html")
 
     @app.route('/poszt')
