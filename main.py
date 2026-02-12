edd3ccffbcd70a3e3923caceab88cb863e1bdce2
asd <asd>
2026-02-12 17:33:10 +0100

    template filters (egyenlore nincs hasznalatban)

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ from flask_login import LoginManager
 from database import init_db, db, User
 from routes import create_routes
 import os
+from datetime import datetime
 
 
 #set cdw to file lokacio
@@ -31,6 +32,31 @@ create_routes(app)
 def load_user(user_id: str):
     return User.query.get(int(user_id))
 
+@app.template_filter('fullTime')
+def datetimeformat(value, format='%Y-%m-%d %H:%M'):
+    if value is None:
+        return ""
+    return value.strftime(format)
+
+@app.template_filter('elapsedTime')
+def datetimeformat(value, format='%Y-%m-%d %H:%M'):
+    if value is None:
+        return ""
+    perc = 60
+    ora = 60*60
+    nap = 60*60*24 
+    honap = 60*60*24*30
+    elasped_seconds = (datetime.now() - value).total_seconds() 
+    if elasped_seconds < perc: 
+        return f"{int(elasped_seconds)} másodperce" 
+    elif elasped_seconds < ora: 
+        return f"{int(elasped_seconds // perc)} perce" 
+    elif elasped_seconds < nap: 
+        return f"{int(elasped_seconds // ora)} órája" 
+    elif elasped_seconds < honap: 
+        return f"{int(elasped_seconds // nap)} napja"
+    else:
+        return f"{int(elasped_seconds // honap)} hónapja"
 
 if __name__ == '__main__':
     app.run(debug=True)#debug=True fejlesztes idejere
